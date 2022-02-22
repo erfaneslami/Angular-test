679c139ee427eb33bf9413ad9e9eb6b1d3326a5c
erfan eslami <erfan.88.eslami@gmail.com>
2022-02-22 20:39:45 +0330

    refactor disabled btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  userName: string;
-
-  checkUsername() {
-    return this.userName !== '' ? true : false;
-  }
+  userName: string = '';
 
   onEmptyUsername() {
     this.userName = '';
