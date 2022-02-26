896e63c8f88766dc34a931cde7a2f014cf7c8b0a
erfan eslami <erfan.88.eslami@gmail.com>
2022-02-26 19:40:38 +0330

    add ability to add ingredient

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.scss']
+  styleUrls: ['./shopping-list-edit.component.scss'],
 })
 export class ShoppingListEditComponent implements OnInit {
+  @Output() IngredientAdded = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAddIngredient(name, amount) {
+    this.IngredientAdded.emit(new Ingredient(name.value, amount.value));
   }
-
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,4 +14,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  setIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
