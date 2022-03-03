aeb6b983b5725b10b9f4e60f673434134593bd17
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-03 19:39:22 +0330

    remove old listeners

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.scss'],
 })
-export class headerComponent {
-  @Output() recipesClicked = new EventEmitter<boolean>();
-  @Output() shoppingListClicked = new EventEmitter<boolean>();
-
-  onRecipesHandler() {
-    this.recipesClicked.emit(true);
-  }
-
-  onShoppingListHandler() {
-    this.shoppingListClicked.emit(true);
-  }
-}
+export class headerComponent {}
